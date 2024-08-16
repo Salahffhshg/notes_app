2a3ccc169208ad82292e752e733c756e544daea6
Salahffhshg <salahcccc17@gmail.com>
2024-08-16 23:06:29 +0300

    AppBar

--- lib/notes.dart
+++ lib/notes.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/notes_view_body.dart';
+
 class Notes extends StatelessWidget {
   const Notes({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: NotesViewBody(),
+
+    );
   }
 }
