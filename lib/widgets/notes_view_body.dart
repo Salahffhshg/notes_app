26626a27a2f77b1ab542d74c75dab9e64a70a44e
Salahffhshg <salahcccc17@gmail.com>
2024-08-17 03:55:18 +0300

    commed

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
+import 'custim_note_item.dart';
 import 'custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -17,53 +16,6 @@ class NotesViewBody extends StatelessWidget {
           ),
           CustomAppBar(),
           NoteItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: const Text(
-              'Buid your carear with salah mohamed',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 30,
-              ),
-            ),
-          ),
-          const Text(
-            'May21 , 2022',
-            style: TextStyle(color: Colors.black),
-          )
         ],
       ),
     );
