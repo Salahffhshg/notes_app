7c06501f092bdb8ddaa267aa5889801b3f9a28d9
Salahffhshg <salahcccc17@gmail.com>
2024-08-18 03:20:40 +0300

    finsh nots buttom shet

--- lib/widgets/add_new_bottem.dart
+++ lib/widgets/add_new_bottem.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:notes/widgets/custom_text_field.dart';
 
+import 'cutom_buttom.dart';
+
 class AddNote extends StatelessWidget {
   const AddNote({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(
@@ -23,7 +26,15 @@ class AddNote extends StatelessWidget {
               hint: 'Content',
               maxlines: 5,
             ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButtom(),
+            SizedBox(height: 18,),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
+
