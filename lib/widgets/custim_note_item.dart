29d2bbe8bfef297081640f3d650b80e8da0bd615
Salahffhshg <salahcccc17@gmail.com>
2024-08-18 03:30:49 +0300

    creat

--- lib/widgets/custim_note_item.dart
+++ lib/widgets/custim_note_item.dart
@@ -1,62 +1,70 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'edit_note_view_body.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                bottom: 16,
-              ),
-              child: Text(
-                'Buid your carear with salah mohamed',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteViewBody();
+        },),);
+      },
+      child: Container(
+        padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(.5),
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  bottom: 16,
+                ),
+                child: Text(
+                  'Buid your carear with salah mohamed',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 26,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 26,
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May21 , 2022',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.4),
+                  fontSize: 16,
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May21 , 2022',
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-                fontSize: 16,
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
