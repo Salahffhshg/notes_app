c6041a697eecfefe0cb67c73a6791c25c1effd4f
Salahffhshg <salahcccc17@gmail.com>
2024-08-19 22:36:29 +0300

    Add note stste

--- lib/cubits/nots_cubit.dart
+++ lib/cubits/nots_cubit.dart
@@ -1,8 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
+
 part 'nots_state.dart';
 
-class NotesCubit extends Cubit<NotsState> {
-  NotesCubit() : super(NotesInitial());
+class NotesCubit extends Cubit<AddNotsState> {
+  NotesCubit() : super(AddNotesInitial());
 }
--- lib/cubits/nots_state.dart
+++ lib/cubits/nots_state.dart
@@ -1,6 +1,16 @@
 part of 'nots_cubit.dart';
-@immutable 
-abstract class NotsState {
-  
+
+@immutable
+abstract class AddNotsState {}
+
+class AddNotesInitial extends AddNotsState {}
+
+class AddNoteLoading extends AddNotsState {}
+
+class AddNoteSuccess extends AddNotsState {}
+
+class AddNoteFailure extends AddNotsState {
+  final String errMessage;
+
+  AddNoteFailure(this.errMessage);
 }
-class NotesInitial extends NotsState{}
